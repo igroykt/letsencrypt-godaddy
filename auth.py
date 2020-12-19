644b131890af4b249da7977b9e1f604fd264fb14
igroykt <artofmotion.igroman@gmail.com>
2020-12-19 15:00:02 +0900

    add_record update

--- auth.py
+++ auth.py
@@ -20,6 +20,7 @@ TTL = int(config.get('GENERAL', 'TTL'))
 SLEEP = int(config.get('GENERAL', 'SLEEP'))
 RETRIES = int(config.get('GENERAL', 'RETRIES'))
 CERTBOT_DOMAIN = os.getenv('CERTBOT_DOMAIN')
+CERTBOT_ALL_DOMAINS = os.getenv('CERTBOT_ALL_DOMAINS').split(",")
 CERTBOT_VALIDATION = os.getenv('CERTBOT_VALIDATION')
 
 LOG_FILE = script_dir + "/auth.log"
@@ -35,15 +36,37 @@ try:
 except Exception as err:
     logging.error(f"Account config error: {err}")
 
-try:
-    client.add_record(CERTBOT_DOMAIN, {'data':CERTBOT_VALIDATION,'name':f'_acme-challenge','ttl':TTL, 'type':'TXT'})
-except Exception as err:
-    logging.error(f"client.add_record error: {err}")
-    if "UNABLE_TO_AUTHENTICATE" in err:
-        sys.exit(1)
+def domainTail(domain):
+    domain = domain.split(".")
+    domain = domain[:len(domain)-2]
+    tmp = []
+    for level in domain:
+        if "*" not in level:
+            tmp.append(level)
+    domain = '.'.join(tmp)
+    if domain:
+        return domain
+    return False
+    
+for current_domain in CERTBOT_ALL_DOMAINS:
+    try:
+        current_domain = current_domain.strip()
+        if len(current_domain.split(".")) > 2:
+            domain_tail = domainTail(current_domain)
+            if domain_tail:
+                client.add_record(CERTBOT_DOMAIN, {'data':CERTBOT_VALIDATION,'name':f'_acme-challenge.{domain_tail}','ttl':TTL,'type':'TXT'})
+            client.add_record(CERTBOT_DOMAIN, {'data':CERTBOT_VALIDATION,'name':f'_acme-challenge','ttl':TTL, 'type':'TXT'})
+    except Exception as err:
+            logging.error(f"client.add_record error: {err}")
+            if "UNABLE_TO_AUTHENTICATE" in err:
+                sys.exit(1)
 
 resolver = dns.resolver.Resolver(configure = False)
-resolver.nameservers = ['8.8.8.8']
+answers = dns.resolver.query(CERTBOT_DOMAIN, 'NS')
+for rdata in answers:
+    rdata = str(rdata)[:-1]
+    break
+resolver.nameservers = [rdata]
 
 n = 1
 while n <= RETRIES:
